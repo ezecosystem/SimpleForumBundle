b73f5471869187986271dbb8fc03f6df1accb9ec
jobou <jobou@sarin>
2013-03-16 18:48:58 +0100

    add fixtures for easier development

---
+++ DataFixtures/ORM/LoadCategoryData.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Jb\SimpleForumBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Jb\SimpleForumBundle\Entity\ForumCategory;
+
+class LoadCategoryData extends AbstractFixture implements OrderedFixtureInterface
+{
+
+  /**
+   * {@inheritDoc}
+   */
+  public function load(ObjectManager $manager)
+  {
+    $list = array(
+            array(
+                    'name' => 'Forum',
+            ),
+            array(
+                    'name' => 'Film',
+                    'parent' => 'Forum',
+            ),
+            array(
+                    'name' => 'Science-Fiction',
+                    'parent' => 'Film',
+            ),
+            array(
+                    'name' => 'Series',
+                    'parent' => 'Forum',
+            ),
+    );
+
+    foreach ($list as $forum) {
+        $element = new ForumCategory();
+        $element->setName($forum['name']);
+        
+        if (isset($forum['parent'])) 
+        {
+            $element->setParent($this->getReference('forum-'.$forum['parent']));
+            $pathString = $this->getReference('forum-'.$forum['parent'])->getPathString();
+            if (is_null($pathString)) {
+                $pathString = '/'.$this->getReference('forum-'.$forum['parent'])->getId().'/';
+            } else {
+                $pathString .= $this->getReference('forum-'.$forum['parent'])->getId().'/';
+            }
+            $element->setPathString($pathString);
+        }
+        $manager->persist($element);
+        $manager->flush();
+        
+        $this->setReference('forum-'.$forum['name'], $element);
+    }
+  }
+
+  public function getOrder()
+  {
+    return 1;
+  }
+}
